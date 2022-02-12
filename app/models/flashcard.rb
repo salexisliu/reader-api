96910945909c7a6b68dd41ab9a4d03eb031370f3
Alexis S Liu <syl1350@gmail.com>
2022-02-12 17:27:44 -0500

    Spacing

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,5 +1,5 @@
 class Flashcard < ApplicationRecord
   belongs_to :book
   belongs_to :user
-  validates :term, uniqueness: { scope: [:book_id], message: "duplicate card"}
+  validates :term, uniqueness: { scope: [:book_id], message: "duplicate card" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, uniqueness: { case_sensitive: false}, presence: true
   validates :password, presence: true
-   has_many :flashcards
+  has_many :flashcards
   has_many :books
 end
